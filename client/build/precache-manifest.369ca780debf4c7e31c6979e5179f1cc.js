f4d7e2b40545511bcfa2324fa1b024ed1d1d5e38
Ufimtsev Lev <l.ufimtsev@itransition.com>
2020-10-20 22:56:48 +0300

    New build fix

---
+++ client/build/precache-manifest.369ca780debf4c7e31c6979e5179f1cc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5afbdb44847c991bbbd0480a93ff85bd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a11147b41f0460971681",
+    "url": "/static/css/2.53159467.chunk.css"
+  },
+  {
+    "revision": "4ffb6d104ee141bd6727",
+    "url": "/static/css/main.55b97930.chunk.css"
+  },
+  {
+    "revision": "a11147b41f0460971681",
+    "url": "/static/js/2.d485fb8c.chunk.js"
+  },
+  {
+    "revision": "39123c4212f521006180b7022b3430d2",
+    "url": "/static/js/2.d485fb8c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ffb6d104ee141bd6727",
+    "url": "/static/js/main.4358efc1.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
